20fd5d0e90bde2ec8babfa3cac4c6fb42978f4b7
Duda Rodrigues <mdudarodrigues.contato@gmail.com>
2025-04-14 13:02:39 -0300

    initial project commit.

---
+++ crud.py
@@ -0,0 +1,48 @@
+def create():
+
+    print(" " * 35)
+    print("|" + "=" * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 24 + "CRIAR REGISTROS:" + " " * 23 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + " " * 18 + "1. Adicionar evento temporal" + " " * 17 + "|")
+    print("|" + " " * 16 + "2. Adicionar um evento histórico" + " " * 15 + "|")
+    print("|" + " " * 20 + "3. Adicionar uma prisão" + " " * 20 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + "=" * 63 + "|")
+    print(" " * 35)
+
+
+def read():
+
+    print(" " * 35)
+    print("|" + "=" * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 23 + "LISTAR REGISTROS:" + " " * 23 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + " " * 13 + "1. Listar todos os eventos históricos" + " " * 13 + "|")
+    print("|" + " " * 13 + "2. Listar eventos históricos por era" + " " * 14 + "|")
+    print("|" + " " * 14 + "3. Listar todas as viagens no tempo" + " " * 14 + "|")
+    print("|" + " " * 11 + "4. Listar todas as viagens de um viajante" + " " * 11 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + "=" * 63 + "|")
+    print(" " * 35)
+
+def update():
+
+    print(" " * 35)
+    print("|" + "=" * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 21 + "ATUALIZAR REGISTROS:" + " " * 22 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + " " * 3 + "1. Atualizar evento para registrar interferência corporal" + " " * 3 + "|")
+    print("|" + " " * 15 + "1. Atualizar reputação do viajante" + " " * 14 + "|")
+    print("|" + " " * 63 + "|")
+    print("|" + "-" * 63 + "|")
+    print("|" + "=" * 63 + "|")
+    print(" " * 35)
